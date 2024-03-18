c0c160f076ded0f8523bf9f359fba8e80304914e
s27743 <s27743@pjwstk.edu.pl>
2024-03-18 11:22:55 +0100

    Implemented max

--- App/Program.cs
+++ App/Program.cs
@@ -4,6 +4,7 @@ var y = 1;
 var z = 2;
 
 Console.WriteLine(Average([1, 2, 3, 4]));
+Console.WriteLine(Max([1, 2, 3, 4]));
 return;
 
 static float Average(int[] arr)
@@ -15,4 +16,18 @@ static float Average(int[] arr)
     }
 
     return sum / arr.Length;
+}
+
+static float Max(int[] arr)
+{
+    var max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
 }
