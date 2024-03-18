10a02925f37bd5ae2264e9658919501e9ccafc77
s27743 <s27743@pjwstk.edu.pl>
2024-03-18 11:09:39 +0100

    Implemented feature

--- App/Program.cs
+++ App/Program.cs
@@ -2,3 +2,17 @@
 var x = 0;
 var y = 1;
 var z = 2;
+
+Console.WriteLine(Average([1, 2, 3, 4]));
+return;
+
+static float Average(int[] arr)
+{
+    var sum = 0f;
+    foreach (int value in arr)
+    {
+        sum += value;
+    }
+
+    return sum / arr.Length;
+}
